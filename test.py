7a329cac74c0358ca351a9b3b91741e6f90f938b
Leonard Papenmeier <lpapenme@D-3140L16.WIWI.UNI-MUENSTER.DE>
2025-10-17 12:42:57 +0200

    runnable with arm and newer python

--- test.py
+++ test.py
@@ -1,18 +1,14 @@
-import tensorflow as tf
+import numpy as np
 
-
-from hpobench.benchmarks.ml import XGBoostBenchmark, TabularBenchmark
-from hpobench.benchmarks.ml.rf_benchmark import RandomForestBenchmark
 from hpobench.benchmarks.nas.tabular_benchmarks import NavalPropulsionBenchmarkOriginal
 
 if __name__ == '__main__':
-
-
-
-
     b = NavalPropulsionBenchmarkOriginal()
     config = b.get_configuration_space(seed=1).sample_configuration()
     print(config)
+    # make all int64 values into int
+    config = {k: v.item() if isinstance(v, np.int64) or isinstance(v, np.float64) else v for k, v in config.items()}
+
     print(b.get_configuration_space(1))
     result_dict = b.objective_function(configuration=config, rng=1)
     print(result_dict)
